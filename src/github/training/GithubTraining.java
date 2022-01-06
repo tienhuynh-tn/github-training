7ec96281320b1680e5252dfc6603e1c383c071ad
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2022-01-06 08:57:53 +0700

    Tien - first commit

---
+++ src/github/training/GithubTraining.java
@@ -0,0 +1,21 @@
+/*
+ * © 2022 tienhuynh-tn
+ * All rights reserved!
+ * For more information, please contact via my email: tien.huynhlt.tn@gmail.com
+ */
+package github.training;
+
+/**
+ *
+ * @author Tien Huynh - Tien Huynh TN - Huynh Le Thuy Tien
+ */
+public class GithubTraining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tien la tui");
+    }
+    
+}
